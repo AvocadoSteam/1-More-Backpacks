1f74cfb884f5d15a55cd5b7cdc693fca9980d0e5
Delaney H <11yomang11@gmail.com>
2022-08-12 07:51:32 -0700

    +Added Distributions +Added GuidTable *Fixed XML's for all bags *Fixed Recipe, and Item scripts

---
+++ More Backpacks/media/lua/server/items/MoreBackpacks_LootDistributions.lua
@@ -0,0 +1,135 @@
+require 'Items/ProceduralDistributions'
+
+--Hunting Backpack
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, 3);
+
+table.insert(ProceduralDistributions.list["HuntingLockers"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["HuntingLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingStoreGear"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreGear"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingLockers"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CampingLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, 12);
+
+table.insert(ProceduralDistributions.list["CampingStoreBackpacks"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreBackpacks"].items, 8);
+
+table.insert(ProceduralDistributions.list["FishingStoreGear"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["FishingStoreGear"].items, 8);
+
+table.insert(ProceduralDistributions.list["CrateCamping"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CrateCamping"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateFishing"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CrateFishing"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, 3);
+
+table.insert(ProceduralDistributions.list["SurvivalGear"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["SurvivalGear"].items, 12);
+
+table.insert(ProceduralDistributions.list["Trapper"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["Trapper"].items, 6);
+
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, 3);
+
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, "MoreBackpacks.HuntingBackpack");
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, 3);
+
+
+
+--Hydration Backpack
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, 3);
+
+table.insert(ProceduralDistributions.list["HuntingLockers"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["HuntingLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingStoreGear"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreGear"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingLockers"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CampingLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreClothes"].items, 12);
+
+table.insert(ProceduralDistributions.list["CampingStoreBackpacks"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CampingStoreBackpacks"].items, 8);
+
+table.insert(ProceduralDistributions.list["FishingStoreGear"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["FishingStoreGear"].items, 8);
+
+table.insert(ProceduralDistributions.list["CrateCamping"].items, "MoreBackpacks.Hydrationackpack");
+table.insert(ProceduralDistributions.list["CrateCamping"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateFishing"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CrateFishing"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, 3);
+
+table.insert(ProceduralDistributions.list["SurvivalGear"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["SurvivalGear"].items, 12);
+
+table.insert(ProceduralDistributions.list["Trapper"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["Trapper"].items, 6);
+
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, 3);
+
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, "MoreBackpacks.HydrationBackpack");
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, 3);
+
+
+--Skater Backpack
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, 3);
+
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, 3);
+
+table.insert(ProceduralDistributions.list["Locker"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["Locker"].items, 3);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["PoolLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["PoolLockers"].items, 3);
+
+
+--Compression Backpack
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["BedRoomDresser"].items, 3);
+
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["DresserGeneric"].items, 3);
+
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["CrateClothesRandom"].items, 3);
+
+table.insert(ProceduralDistributions.list["Locker"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["Locker"].items, 3);
+
+table.insert(ProceduralDistributions.list["GymLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["GymLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 3);
+
+table.insert(ProceduralDistributions.list["PoolLockers"].items, "MoreBackpacks.CompressionBackpack");
+table.insert(ProceduralDistributions.list["PoolLockers"].items, 3);
